4de17752fc5992351ce6a778401dc111570e090e
Ben <ben.lynbrook@gmail.com>
2019-04-21 18:00:35 -0700

    mouse wip
    mouse wip

---
+++ code/Mouse.cpp
@@ -0,0 +1,132 @@
+#include <iostream>
+#include <limits>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <cstdlib>
+#include <vector>
+// #include <stdio.h>      /* printf, scanf, puts, NULL */
+// #include <stdlib.h>     /* srand, rand */
+// #include <time.h>       /* time */
+#include "Mouse.h"
+
+using namespace std;
+
+Mouse::Mouse(float x, float y, float z, float w, float h, float r, float g, float b): z(z) {
+    for(int i = 0; i < 4; i++) {
+        string filename = "sprites/" + to_string(i) + ".png";
+        cout << "filename:\t" << filename << endl;
+        animation.push_back(new AnimatedRect(filename.c_str(), 2, 2, 50, true, true, x, y, w, h));
+        animation[i]->setR(r);
+        animation[i]->setG(g);
+        animation[i]->setB(b);
+    }
+}
+
+Mouse::Mouse(float x, float y, float z, float w, float h): z(z) {
+    for(int i = 0; i < 4; i++) {
+        string filename = "sprites/" + to_string(i) + ".png";
+        cout << "filename:\t" << filename << endl;
+        animation.push_back(new AnimatedRect(filename.c_str(), 2, 2, 50, true, true, x, y, w, h));
+    }
+}
+
+Mouse::~Mouse() {
+    for(int i = 0; i < animation.size(); i++) {
+        delete animation[i];
+    }
+}
+
+float Mouse::getX() const {
+    return animation[0]->getX();
+}
+
+float Mouse::getY() const {
+    return animation[0]->getY();
+}
+
+float Mouse::getW() const {
+    return animation[0]->getW();
+}
+
+float Mouse::getH() const {
+    return animation[0]->getH();
+}
+
+float Mouse::getR() const {
+    return animation[0]->getR();
+}
+
+float Mouse::getG() const {
+    return animation[0]->getG();
+}
+
+float Mouse::getB() const {
+    return animation[0]->getB();
+}
+
+void Mouse::setX(float x) {
+    for(int i = 0; i < animation.size(); i++) {
+        animation[i]->setX(x);
+    }
+}
+
+void Mouse::setY(float y) {
+    for(int i = 0; i < animation.size(); i++) {
+        animation[i]->setY(y);
+    }
+}
+
+void Mouse::setW(float w) {
+    for(int i = 0; i < animation.size(); i++) {
+        animation[i]->setW(w);
+    }
+}
+
+void Mouse::setH(float h) {
+    for(int i = 0; i < animation.size(); i++) {
+        animation[i]->setH(h);
+    }
+}
+
+void Mouse::setR(float r) {
+    for(int i = 0; i < animation.size(); i++) {
+        animation[i]->setR(r);
+    }
+}
+
+void Mouse::setG(float g) {
+    for(int i = 0; i < animation.size(); i++) {
+        animation[i]->setG(g);
+    }
+}
+
+void Mouse::setB(float b) {
+    for(int i = 0; i < animation.size(); i++) {
+        animation[i]->setB(b);
+    }
+}
+
+bool Mouse::contains(float px, float py) const {
+    return animation[0]->contains(px, py);
+}
+
+void Mouse::draw(Direction dir) {
+    animation[translate(dir)]->draw();
+}
+
+int Mouse::translate(Direction dir) {
+    switch (dir) {
+        case Left:
+            return 0;
+        case Up:
+            return 1;
+        case Right:
+            return 2;
+        case Down:
+            return 3;
+        default:
+            cout << "ERROR WRONG DIRECTION TYPE!" << endl;
+            return -1;
+    }
+}
---
+++ code/Mouse.h
@@ -0,0 +1,50 @@
+#ifndef Mouse_h
+#define Mouse_h
+
+#include "GlutApp.h"
+#include "AnimatedRect.h"
+#include "TexRect.h"
+#include "vector"
+
+
+class Mouse {
+// float x, y, w, h, r, g, b;
+float z;
+vector<AnimatedRect*> animation;
+
+enum Direction { Left, Up, Right, Down};  //0, 1, 2, 3
+    
+public:
+    
+    Mouse(float=0.0f, float=0.0f, float=0.5f, float=0.4f, float=0.2f, float=1.0f, float=1.0f, float=1.0f));
+    Mouse(float=0.0f, float=0.0f, float=0.5f, float=0.1f, float=0.1f);
+
+    float getX() const;
+    float getY() const;
+    float getW() const;
+    float getH() const;
+    
+    float getR() const;
+    float getG() const;
+    float getB() const;
+    
+    void setX(float);
+    void setY(float);
+    void setW(float);
+    void setH(float);
+    
+    void setR(float);
+    void setG(float);
+    void setB(float);
+
+    bool contains(float, float) const;
+    
+    void draw(Direction dir);
+
+    int translate(Direction dir);
+
+    ~Mouse();
+    
+};
+
+#endif
