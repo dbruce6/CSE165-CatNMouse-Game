04d9ed60a037ee3626de61d3205a35eee967f1ff
Bruce <bduong6@ucmerced.edu>
2019-05-12 21:06:03 -0700

    wip
    wip

--- code/App.h
+++ code/App.h
@@ -23,6 +23,7 @@ std::vector<TexRect*> obstacle;
 
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 // Game editor stuff:
 std::vector<Rect*> mapMaking;
 int mapCounter = 0;
@@ -32,6 +33,8 @@ float rectHeight, rectWidth;
 bool black = true;
 bool m1 = false;
 float m1x, m1y;
+=======
+>>>>>>> parent of d42d1f3... full textures for everything
 =======
 >>>>>>> parent of d42d1f3... full textures for everything
 =======
