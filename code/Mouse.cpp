b2f64b2f06333d624021730f4e20bcbfa397b97b
Ben <ben.lynbrook@gmail.com>
2019-05-15 17:39:39 -0700

    wip

--- code/Mouse.cpp
+++ code/Mouse.cpp
@@ -13,7 +13,24 @@
 using namespace std;
 
 Mouse::Mouse(float x, float y, float z, float w, float h, float r, float g, float b): Animal("images/mouse/", x, y, 1.0f, 0.1f, 0.1f) {
+    moving = false;
     for(int i = 0; i < 4; i++) {
         // TODO
+    }
+}
+
+bool Mouse::getMoving() {
+    return moving;
+}
+
+void Mouse::setMoving(bool move) {
+    moving = move;
+}
+
+void Mouse::draw(int dir) {
+    if(moving) {
+        Animal::draw(dir);
+    } else {
+        notMoving[dir]->draw(getZ());
     }
 }
--- code/Mouse.h
+++ code/Mouse.h
@@ -9,11 +9,17 @@
 
 
 class Mouse : public Animal {
+    bool moving;
+    std::vector<TexRect*> notMoving;
 public:
     Mouse(float=0.0f, float=0.0f, float=0.5f, float=0.4f, float=0.2f, float=1.0f, float=1.0f, float=1.0f);
 
-    ~Mouse();
+    bool getMoving();
+    void setMoving(bool);
+
+    void draw(int);
     
+    ~Mouse();
 };
 
 #endif
